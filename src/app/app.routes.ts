961dde6bdc8d7f155573eb03e3dde32d20a4ae7e
onigra <3280467rec@gmail.com>
2025-06-02 16:28:15 +0900

    ルーティング追加

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { UrlLinkConverterComponent } from './url-link-converter/url-link-converter.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'url-converter',
+        component: UrlLinkConverterComponent
+    },
+    {
+        path: '',
+        redirectTo: 'url-converter',
+        pathMatch: 'full'
+    }
+];
